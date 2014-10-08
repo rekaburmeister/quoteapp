29e50e033db28e51bc0bf0c3a0922adf8634c142
unknown <reka.burmeister@TST-REKA.red-gate.com>
2014-10-08 15:35:04 +0100

    Initial commit

---
+++ QuoteApp/Controllers/CustomerController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuoteApp.Models;
+using RazorPDF;
+
+namespace QuoteApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Customer/
+        public ActionResult Index()
+        {
+            //return View(db.Customers.ToList());
+            List<Customer> customers = new List<Customer>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Customer customer = new Customer
+                {
+                    CustomerID = i,
+                    FirstName = string.Format("FirstName{0}", i.ToString()),
+                    LastName = string.Format("LastName{0}", i.ToString())
+                };
+                customers.Add(customer);
+            }
+            return View(customers);
+        }
+
+        public PdfResult PDF()
+        {
+            List<Customer> customers = new List<Customer>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Customer customer = new Customer
+                {
+                    CustomerID = i,
+                    FirstName = string.Format("FirstName{0}", i.ToString()),
+                    LastName = string.Format("LastName{0}", i.ToString())
+                };
+                customers.Add(customer);
+            }
+
+            return new RazorPDF.PdfResult(customers, "PDF");
+        }
+
+        // GET: /Customer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: /Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Customer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="CustomerID,FirstName,LastName")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: /Customer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: /Customer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="CustomerID,FirstName,LastName")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: /Customer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: /Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
