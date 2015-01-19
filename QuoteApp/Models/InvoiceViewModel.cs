8be24b733805786d45c89860d2cf8285c3f9418b
Reka Burmeister <reka.burmeister@gmail.com>
2015-01-19 15:50:35 +0000

    Updating InvoiceViewModel with Required fields plus adding the ContactId and WorkLocationId initializes

--- QuoteApp/Models/InvoiceViewModel.cs
+++ QuoteApp/Models/InvoiceViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,15 +8,40 @@ namespace QuoteApp.Models
 {
     public class InvoiceViewModel
     {
+        [Required]
         public string InvoiceId { get; set; }
+
+        [Required]
         public string Date { get; set; }
+
+        [Required]
+        public int WorkLocationId { get; set; }
+
+        [Required]
         public string InvoiceTo { get; set; }
+
+        [Required]
         public string InvoiceToAddress { get; set; }
+        
         public string Reference { get; set; }
+
+        [Required]
+        public int ContactId { get; set; }
+
+        [Required]
         public string CareOf { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string CareOfEmail { get; set; }
+
+        [Required]
         public string CareOfNumber { get; set; }
+
+        [Required]
         public string Details { get; set; }
+
+        [Required]
         public int Price { get; set; }
 
         public InvoiceViewModel()
@@ -32,6 +58,8 @@ namespace QuoteApp.Models
             InvoiceTo = quote.WorkLocation.WorkLocationName;
             InvoiceToAddress = quote.WorkLocation.GetAddress();
             Details = "Refurbishment to squash courts as agrred";
+            ContactId = quote.ContactId;
+            WorkLocationId = quote.WorkLocationId;
             CareOf = quote.Contact.GetName();
             CareOfEmail = quote.Contact.Email;
             CareOfNumber = quote.Contact.MobileNumber ?? quote.Contact.PhoneNumber;
