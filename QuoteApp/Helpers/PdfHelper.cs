d05d25588d7beec4d18367a2c22284eb76f2c6e3
Reka Burmeister <reka.burmeister@gmail.com>
2015-01-28 09:40:38 +0000

    Trying to get pdf working with pdfcrowd

--- QuoteApp/Helpers/PdfHelper.cs
+++ QuoteApp/Helpers/PdfHelper.cs
@@ -7,19 +7,18 @@ namespace QuoteApp.Helpers
 {
     public static class PdfHelper
     {
-        public static string FormatBasePdf(string html, string basePath)
+        public static string FormatBasePdf(string html)
         {
-            basePath = basePath.Replace('\\', '/');
             html = string.Format(@"<!DOCTYPE html>
 <html>
 <head>
     <meta charset=""utf-8"" />
     <meta name=""viewport"" content=""width=device-width"", initial-scale=""1.0"">
-    <link href=""{0}Content/bootstrap.css"" rel=""stylesheet""/>
-<link href=""{0}Content/site.css"" rel=""stylesheet""/>
+    <link href=""bootstrap.min.css"" rel=""stylesheet""/>
+<link href=""site.css"" rel=""stylesheet""/>
 </head>
-<body>{1}</body></html>", basePath, html);
-            html = html.Replace("src=\"/Content/Images/", string.Format("src=\"{0}/Content/Images/", basePath));
+<body>{0}</body></html>", html);
+            html = html.Replace("src=\"/Content/Images/", "src=\"");
             return html;
         }
     }
